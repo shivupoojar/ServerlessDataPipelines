20e5c2ce4cb9feec428bdaf7a437c88fff5dbca6
shivupoojar <shivananda.poojara@ritindia.edu>
2021-01-09 07:58:58 +0200

    Update handler.sh

--- ApacheNifi-SDP/functions/bashffmpeg/handler.sh
+++ ApacheNifi-SDP/functions/bashffmpeg/handler.sh
@@ -6,9 +6,6 @@ export nano=`date +%s%N`
 export OUT=/tmp/$nano.mp4
 export result=$nano
 
-# Save stdin to a temp file
-
-
 while IFS='=' read -r -d '&' key value; do
    export name=$key
 done <<EOF
@@ -17,13 +14,7 @@ EOF
 cat - > ${OUT}
 ffmpeg -loglevel info -nostats -i ${OUT}  -vf fps=15  "${name}_%03d.jpg" < /dev/null
 tar cf - *.jpg 
-#find -name "*.jpg" | xargs tar cf - -T 
 rm ${OUT}
 rm *.jpg
-#for filename in $(ls *.jpg); do
-
- #   curl -i -H "name:$filename" --data-binary  @$filename -X POST  http://192.168.1.249:8001/contentListener
-    
-#done
 
 
